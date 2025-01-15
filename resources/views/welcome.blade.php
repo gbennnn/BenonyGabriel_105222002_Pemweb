abfea8c6805e601eea8ed481a584b2a724f144ea
Benony Gabriel <gbenn.work@gmail.com>
2025-01-16 00:11:23 +0700

    Update welcome view with new title and enhanced layout for user profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>UAS Pemrograman Web</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
-    <h1>Welcome</h1>
+    <div class="header">
+        <h1>UAS Pemrograman Web</h1>
+    </div>
+
+    <div class="content">
+        <div class="profile-picture">
+            <p>Foto profil</p>
+        </div>
+        <h2>Benony Gabriel</h2>
+        <p class="nim">NIM: 105222002</p>
+        <button class="btn">Demo Project</button>
+    </div>
 </body>
 
 </html>
