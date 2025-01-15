15e924b751fa48c676b196042ddc61bcb52ea5bf
Benony Gabriel <gbenn.work@gmail.com>
2025-01-15 22:20:48 +0700

    Add success and error message alerts to the layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -48,6 +48,23 @@
 
 <body>
 
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
     <!-- Sidenav -->
     <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
         <div class="scrollbar-inner">
