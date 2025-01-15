7b2d446c853f8d31c9605093540733d807b6b24b
Benony Gabriel <gbenn.work@gmail.com>
2025-01-16 00:58:24 +0700

    Refactor header and content structure; update styles for improved layout and social links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,21 +7,33 @@
     <title>UAS Pemrograman Web</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
 </head>
 
 <body>
-    <div class="header">
+    <header>
         <h1>UAS Pemrograman Web</h1>
-    </div>
+    </header>
 
-    <div class="content">
+    <main class="content">
         <div class="profile-picture">
             <img src="{{ asset('img/profile-pic-benony.png') }}" alt="Profile Picture Benony">
         </div>
         <h2>Benony Gabriel</h2>
         <p class="nim">NIM: 105222002</p>
-        <button class="btn">Demo Project</button>
-    </div>
+        <a class="btn" href="{{ route('event.home') }}">Demo Project</a>
+
+
+        <!-- Social Links -->
+        <div class="social-links" style="margin-top: 50px;">
+            <p>Source Code:</p>
+            <a href="https://github.com/gbennnn/BenonyGabriel_105222002_Pemweb" target="_blank">
+                <i class="ri-github-fill ri-2x"></i>
+            </a>
+        </div>
+    </main>
+
 </body>
 
 </html>
