a6e89de047a0be81c2ab978d9d6295ad0fee17e3
Benony Gabriel <gbenn.work@gmail.com>
2025-01-16 13:39:50 +0700

    Add edit modal for event updates with form handling

--- resources/views/jawaban/NomorTiga/index.blade.php
+++ resources/views/jawaban/NomorTiga/index.blade.php
@@ -35,4 +35,32 @@
 {{-- Modal Edit --}}
 
 <!-- Gunakan tag form dibawah ini untuk submit data jadwal yang akan diupdate. Gunakan contoh modal yang sudah dibuat pada nomor 1 dan 2 sebagai referensi -->
-<!-- <form class="modal-content" method="POST" action="{{ route('event.update') }}"> </form> -->
+@foreach ($events as $event)
+    <div class="modal fade" id="editModal-{{ $event->id }}" tabindex="-1" role="dialog"
+        aria-labelledby="editModalLabel-{{ $event->id }}" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <form class="modal-content" method="POST" action="{{ route('event.update') }}">
+                @csrf
+                <input type="hidden" name="id" value="{{ $event->id }}">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel-{{ $event->id }}">Edit Jadwal</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <label>Event</label>
+                    <input type="text" class="form-control" name="name" required value="{{ $event->name }}">
+                    <label class="mt-2">Start</label>
+                    <input type="date" class="form-control" name="start" required value="{{ $event->start }}">
+                    <label class="mt-2">End</label>
+                    <input type="date" class="form-control" name="end" required value="{{ $event->end }}">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endforeach
