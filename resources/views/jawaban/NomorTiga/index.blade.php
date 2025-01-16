1ad010791473b97965133ab5fb4e00756cf87bed
Benony Gabriel <gbenn.work@gmail.com>
2025-01-16 12:16:08 +0700

    Remove unnecessary blank line in index view for improved readability

--- resources/views/jawaban/NomorTiga/index.blade.php
+++ resources/views/jawaban/NomorTiga/index.blade.php
@@ -29,7 +29,6 @@
             @endforeach
         </tbody>
     @endif
-
 </table>
 
 <!-- Gunakan tag form dibawah ini untuk submit data jadwal yang akan diupdate. Gunakan contoh modal yang sudah dibuat pada nomor 1 dan 2 sebagai referensi -->
